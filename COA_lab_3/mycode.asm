b8035a48bf1b259497d142b5e1aa61e95fa8cc5a
Abhijeet Panigrahi <141105280+AbhijeetPanigrahi@users.noreply.github.com>
2024-03-30 17:01:33 +0530

    Add files via upload

---
+++ COA_lab_3/mycode.asm
@@ -0,0 +1,43 @@
+                
+            Assembler 
+                      
+DB: Define byte
+MARKS DB 
+
+DW: 
+MARKS DW 1234H, 4567H
+
+DD: Define Double 
+
+
+DQ
+
+Data1 DD 12345678H
+
+DT: Define Ten Bytes
+
+Data1 DT 123456789ABCDEF34567H
+
+END (End of progamme)
+ENDP (End of procedure)
+ENDS (End of segment) 
+
+EVEN
+
+EQU (Equate)
+LABEL EQU 0500H
+
+EXTRN (external and public)
+MODULES 
+
+LABEL
+CONTINUE LABEL FAR
+
+LENGTH (Byte length of a label)
+MOV CX, LENGTH ARRAY 
+
+LOCAL 
+LOCAL a,b, DATA1, Array, Routine
+
+
+OFFSET
